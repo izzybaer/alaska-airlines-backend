1dfce28f6b9e2e3eb819063d8bc746d1cba68dd2
Izabella Baer <izabellabaer@gmail.com>
2018-02-20 16:55:49 -0800

    added basic location model with create, fetchall, fetchone, and update methods

--- src/model/flight.js
+++ src/model/flight.js
@@ -1,1 +1,22 @@
 'use strict';
+
+import mongoose from 'mongoose';
+
+const flightSchema = mongoose.Schema({
+  arrival: { type: String, required: true },
+  departure: { type: String, required: true },
+  arrivalTime: { type: Number, required: true },
+  flightNumber: { type: Number, required: true },
+  departureTime: { type: Number, required: true },
+  departureCode: { type: String, required: true },
+  destinationCode: { type: String, required: true },
+  coachClassPrice: { type: Number, required: true },
+  coachClassCount: { type: Number, required: true },
+  firstClassPrice: { type: Number, required: true },
+  firstClassCount: { type: Number, required: true },
+  locationId: {type: Schema.Types.ObjectId, ref: 'location'},
+});
+
+const Flight = mongoose.model('flight', flightSchema);
+
+export default Flight;

ebc8c891b1ca8409ed048f180c1d8f2b40ddc051
Izabella Baer <izabellabaer@gmail.com>
2018-02-21 23:18:55 -0800

    location and flight router mostly finished

--- src/route/flight-router.js
+++ src/route/flight-router.js
@@ -1,1 +1,76 @@
 'use strict';
+
+import Flight from '../model/flight';
+import httpErrors from 'http-errors';
+
+const jsonParser = require('body-parser').json();
+const flightRouter = module.exports = new require('express').Router();
+
+flightRouter.post('/api/flights', jsonParser, (req, res, next) => {
+    console.log('hit POST /api/flights');
+
+    return new Flight(req.body).save()
+        .then(flight => res.json(flight))
+        .catch(next);
+});
+
+flightRouter.get('/api/flights/:id', (req, res, next) => {
+    console.log('hit GET /api/flights/:id');
+
+    return Flight.findById(request.params.id)
+        .populate('location') // izzy - use this with care
+        .then(flight => { // with great power comes great responsibility
+            if(!flight) {
+                throw httpErrors(404, 'flight not found')
+            }
+            return res.json(flight)
+        }).catch(next);
+});
+
+flightRouter.get('/api/flights/', (req, res, next) => {
+    console.log('hit GET /api/flights');
+
+    let allFlights = null;
+
+    return Flight.find({})
+        .then(flights => {
+            allFlights = flights;
+            return Flight.find({});
+        })
+        .then(flightCount => {
+            // izzy- no access to 'flights'
+            let responseData = {
+                count: flightCount,
+                data: allFlights,
+            };
+            res.json(responseData);
+        });
+});
+
+flightRouter.put('/api/flights/:id', jsonParser, (req, res, next) => {
+    console.log('hit PUT /api/flights/:id');
+
+    // izzy - configures mongo update behavior
+    let options = {new: true, runValidators: true};
+
+    // izzy - additional validation before updating
+    return Flight.findByIdAndUpdate(req.params.id, req.body, options)
+        .then(flight => {
+            if(!flight) {
+                throw httpErrors(404, 'flight not found');
+            }
+            return res.json(flight);
+        }).catch(next);
+});
+
+flightRouter.delete('/api/flights/:id', (req, res, next) => {
+     console.log('hit DELETE /api/flights/:id');
+     
+        return Flight.findByIdAndRemove(req.params.id)
+            .then(flight => {
+                if(!flight) {
+                    throw httpErrors(404, 'flight not found');
+                }
+                return res.sendStatus(204);
+            }).catch(next);
+});
--- src/route/location-router.js
+++ src/route/location-router.js
@@ -1,45 +1,63 @@
 'use strict';
 
 import Location from '../model/flight';
+import httpErrors from 'http-errors';
 
 const jsonParser = require('body-parser').json();
+
 const locationRouter = module.exports = new require('express').Router();
 
 locationRouter.post('/api/locations', jsonParser, (req, res, next) => {
     console.log('hit POST /api/locations');
-    new Location(req.body).save()
+    if(!locationName || !airportCode) return next(httpErrors(400, 'location needs a name and airport code'));
+
+    return new Location(req.body).save()
         .then(location => res.json(location))
         .catch(next);
-})
+});
 
 locationRouter.get('/api/locations/:id', (req, res, next) => {
     console.log('hit GET /api/locations/:id');
 
-    Location.findById(req.params.id).populate('flights')
-        .then(location => res.json(location))
+    return Location.findById(req.params.id).populate('flights')
+        .then(location => {
+            if(!location) 
+                throw httpErrors(404, 'location not found');
+            return res.json(location);
+        })
         .catch(next);
-})
+});
 
 locationRouter.get('/api/locations', (req, res, next) => {
     console.log('hit GET /api/locations');
 
-    Location.find({})
-    .then(locations => res.json(locations))
-    .catch(next);
-})
+    return Location.find({})
+        .then(locations => res.json(locations))
+        .catch(next);
+});
 
 locationRouter.put('/api/locations/:id', jsonParser, (req, res, next) => {
     console.log('hit PUT /api/locations/:id');
+      let options = { new: true, runValidators: true };
+      
 
-    Location.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
-        .then(location => res.json(location))
+    return Location.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        .then(location => {
+            if(!location)
+                throw httpErrors(404, 'location not found');
+            return res.json(location);
+        })
         .catch(next);
-})
+});
 
 locationRouter.delete('/api/locations/:id', (req, res, next) => {
     console.log('hit DELETE /api/locations/:id');
     
-    Location.findByIdAndRemove(req.params.id)
-        .then(() => res.sendStatus(204))
+    return Location.findByIdAndRemove(req.params.id)
+        .then(location => {
+            if(!location)
+                throw httpErrors(404, 'location not found');
+            return res.sendStatus(204);
+        })
         .catch(next);
-})
+});

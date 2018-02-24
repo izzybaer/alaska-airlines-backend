763aef9356621ae9a6ebc064b5735bf30b269a12
Izabella Baer <izabellabaer@gmail.com>
2018-02-24 13:05:12 -0800

    scaffolding out GET /api/locations test

--- src/__test__/location-router.test.js
+++ src/__test__/location-router.test.js
@@ -4,16 +4,17 @@ require('./lib/setup');
 
 import server from '../lib/server';
 import superagent from 'superagent';
+import locationMock from './lib/location-mock';
 
 const apiURL = `http://localhost:${process.env.PORT}`;
 
 describe('/api/locations', () => {
     beforeAll(server.start);
     afterAll(server.stop);
-    // afterEach(locationRemove);
+    afterEach(locationMock.remove);
 
     describe('POST /api/locations', () => {
-        it('should return a 200 and a location if there are no errors', () => {
+        it('should respond with a 200 status and a location if there are no errors', () => {
             return superagent.post(`${apiURL}/api/locations`)
                 .send({
                     'Name': 'Baltimore',
@@ -21,7 +22,23 @@ describe('/api/locations', () => {
                 })
                 .then(res => {
                     expect(res.status).toEqual(200);
-                    expect(res.body).toBeTruthy();
+                    expect(res.body.Code).toEqual('BWI');
+                });
+        });
+    });
+
+    describe('GET /api/locations', () => {
+        it('should respond with a 200 status and a location if there are no errors', () => {
+            let tempLocationMock;
+
+            return locationMock.create()
+                .then(location => {
+                    tempLocationMock = location;
+                    return superagent.get(`${apiURL}`);
+                })
+                .then(res => {
+                    expect(res.status).toEqual(200);
+                    expect(JSON.stringify(res.body.Code)).toEqual(JSON.stringify(tempLocationMock.Code));
                 });
         });
     });
